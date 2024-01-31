94f2a2e9750a18be0014de23bd3e44934e24eba9
98saurabh <112603044+98saurabh@users.noreply.github.com>
2024-01-31 09:41:33 +0530

    Update streamli_app.py
    added line to import streamlit
    added a title

--- streamli_app.py
+++ streamli_app.py
@@ -1,4 +1,3 @@
-import streamlit as st
-st.title('hello saurabh')
-st.text('this is my first data app in streamlit_app')
-st.text_input('first name')
+import streamlit 
+streamlit.title('My Parents New Healthy Dinner')
+
